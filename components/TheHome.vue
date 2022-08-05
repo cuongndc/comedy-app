b5fc2a1132d5d62fab048df5b7c849fe6e32eb5e
BaoBao <86643266+cuongndc@users.noreply.github.com>
2022-08-05 20:56:43 +0700

    commit

--- components/TheHome.vue
+++ components/TheHome.vue
@@ -1,11 +1,14 @@
 <script lang="ts" setup>
+import axios from 'axios'
 import type { IHomePage } from '~/types'
 import { HomePageTypes } from '~/types'
-import { useAsyncData } from '#app'
+import { useAsyncData, useRuntimeConfig } from '#app'
 
 import { LazyHomepageNewStory, LazyHomepageRepresentCategory, LazyHomepageSpotlight, LazyHomepageTrending, PulseHomeLoading, SharedBannerBar } from '#components'
-
-const { data: homepages, pending } = await useAsyncData<IHomePage>('home-page', () => $fetch('/api/homepage'))
+const config = useRuntimeConfig()
+const { data: homepages, pending } = await useAsyncData<IHomePage>('home-page', () => axios.get('/api/wb/homepage', {
+  baseURL: config.public.publicURL,
+}).then(res => res.data))
 </script>
 
 <template>
