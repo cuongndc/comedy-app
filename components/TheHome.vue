2548d5329a3c87ecc0dc1d222c648066afbc26ed
BaoBao <86643266+cuongndc@users.noreply.github.com>
2022-08-05 20:12:34 +0700

    Update TheHome.vue

--- components/TheHome.vue
+++ components/TheHome.vue
@@ -10,19 +10,20 @@ const { data: homepages, pending } = await useAsyncData<IHomePage>('home-page', 
   <div v-if="pending" class="bg-white">
     <PulseHomeLoading />
   </div>
-  <main v-else class="h-[auto] bg-white">
-    <div v-for="record in homepages.data" :key="record._id">
-      <LazyHomepageSpotlight
-        v-if="record.type === HomePageTypes._banner"
-        :banner="record"
-      />
-      <SharedBannerBar v-if="record.type === HomePageTypes._menu" />
-      <LazyHomepageTrending v-if="record.type === HomePageTypes._trend" :record="record" />
-      <LazyHomepageRepresentCategory v-if="record.type === HomePageTypes._representCategory" :record="record" />
-      <LazyHomepageNewStory
-        v-if="record.type === HomePageTypes._newest || record.type === HomePageTypes._recommendation"
-        :record="record"
-      />
-    </div>
-  </main>
+  {{homepages}}
+<!--  <main v-else class="h-[auto] bg-white">-->
+<!--    <div v-for="record in homepages.data" :key="record._id">-->
+<!--      <LazyHomepageSpotlight-->
+<!--        v-if="record.type === HomePageTypes._banner"-->
+<!--        :banner="record"-->
+<!--      />-->
+<!--      <SharedBannerBar v-if="record.type === HomePageTypes._menu" />-->
+<!--      <LazyHomepageTrending v-if="record.type === HomePageTypes._trend" :record="record" />-->
+<!--      <LazyHomepageRepresentCategory v-if="record.type === HomePageTypes._representCategory" :record="record" />-->
+<!--      <LazyHomepageNewStory-->
+<!--        v-if="record.type === HomePageTypes._newest || record.type === HomePageTypes._recommendation"-->
+<!--        :record="record"-->
+<!--      />-->
+<!--    </div>-->
+<!--  </main>-->
 </template>
