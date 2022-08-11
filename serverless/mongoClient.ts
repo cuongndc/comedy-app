09294d4958ff138e2896f7a69088db02b4811fac
BaoBao <86643266+cuongndc@users.noreply.github.com>
2022-08-11 11:09:19 +0700

    Update mongoClient.ts

--- serverless/mongoClient.ts
+++ serverless/mongoClient.ts
@@ -7,7 +7,7 @@ const options = {
   useNewUrlParser: true,
 }
 
-if (!process.env.MONGODB_URI)
+if (!uri)
   throw new Error('Please add your Mongo URI to .env.local')
 
 const client = new MongoClient(uri, options as MongoClientOptions)
